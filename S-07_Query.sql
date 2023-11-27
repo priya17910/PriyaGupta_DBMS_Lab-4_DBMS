c1194988bb88f2843adf7ff15c27d31abb4bc985
priya17910 <priyagupta17910@gmail.com>
2023-11-27 13:14:26 +0530

    Update S-07_Query.sql

--- S-07_Query.sql
+++ S-07_Query.sql
@@ -13,4 +13,5 @@ select category.cat_id, cat_name, pro_name, sp1.supp_price
             inner join supplier_pricing sp2 on p2.pro_id = sp2.pro_id
             where p2.cat_id = p1.cat_id
 				and sp2.supp_price < sp1.supp_price
-        );
+        )
+	order by category.cat_id;
